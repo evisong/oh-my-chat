31797eea52fbd2158cde4c457ebede963164478d
EviSONG <evisong@live.com>
2023-11-09 11:56:37 +0800

    第1章 原型

--- src/App.jsx
+++ src/App.jsx
@@ -63,10 +63,34 @@ function App() {
           <button><img src={menuIcon} /></button>
         </header>
         <ul className="message-list">
-          <li className="">
+          <li className="from-me">
             <img src={reactLogo} className="avatar" />
             <p className="message">
               你好React！
+            </p>
+          </li>
+          <li>
+            <img src={reactLogo} className="avatar" />
+            <p className="message">
+              你好React！
+            </p>
+          </li>
+          <li className="from-me">
+            <img src={reactLogo} className="avatar" />
+            <p className="message">
+              欢迎阅读《现代React Web应用设计开发实践》，你现在看到的是本书的样例应用。
+            </p>
+          </li>
+          <li>
+            <img src={reactLogo} className="avatar" />
+            <p className="message">
+              这款应用有名字吗？
+            </p>
+          </li>
+          <li className="from-me">
+            <img src={reactLogo} className="avatar" />
+            <p className="message">
+              有的，就叫《我聊》。
             </p>
           </li>
         </ul>
