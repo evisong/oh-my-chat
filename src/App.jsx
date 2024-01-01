b9c5f6c29d3f0b5b0518c1b83d4cb0147cbff7e1
EviSONG <evisong@live.com>
2024-01-02 00:14:20 +0800

    第2章联系人

--- src/App.jsx
+++ src/App.jsx
@@ -107,37 +107,57 @@ const MessagesPane = () => (
   </>
 );
 
-const ContactsPane = () => (
-  <>
-    <header className="contact-top-menu">
-      <form>
-        <input maxLength={20} />
-        <input type="submit" value="搜索联系人" />
-      </form>
-      <button>添加联系人</button>
-    </header>
-    <ul className="contact-list">
-      <li>
-        <a href="#">
-          <img src={reactLogo} className="avatar" alt="头像" />
-          <div className="contact-name">小帅</div>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <img src={reactLogo} className="avatar" alt="头像" />
-          <div className="contact-name">小白</div>
-        </a>
-      </li>
-      <li className="active">
-        <a href="#">
-          <img src={reactLogo} className="avatar" alt="头像" />
-          <div className="contact-name">小美</div>
-        </a>
-      </li>
-    </ul>
-  </>
-);
+const ContactsPane = () => {
+  const contacts = [
+    {
+      id: 1,
+      name: '小帅',
+    },
+    {
+      id: 2,
+      name: '小白',
+    },
+    {
+      id: 3,
+      name: '小美',
+      active: true,
+    },
+    {
+      id: 4,
+      name: '大壮',
+    },
+    {
+      id: 5,
+      name: '老宋',
+    },
+    {
+      id: 6,
+      name: '贾姐',
+    },
+  ];
+
+  return (
+    <>
+      <header className="contact-top-menu">
+        <form>
+          <input maxLength={20} />
+          <input type="submit" value="搜索联系人" />
+        </form>
+        <button>添加联系人</button>
+      </header>
+      <ul className="contact-list">
+        {contacts.map((contact) => (
+          <li key={contact.id} className={contact.active && 'active'}>
+            <a href="#">
+              <img src={reactLogo} className="avatar" alt="头像" />
+              <div className="contact-name">{contact.name}</div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 const ContactDetail = () => (
   <>
@@ -173,8 +193,12 @@ function App() {
           </li>
         </ul>
       </nav>
-      <aside>{activeView === 'chat' ? <ThreadsPane /> : <ContactsPane />}</aside>
-      <main>{activeView === 'chat' ? <MessagesPane /> : <ContactDetail />}</main>
+      <aside>
+        {activeView === 'chat' ? <ThreadsPane /> : <ContactsPane />}
+      </aside>
+      <main>
+        {activeView === 'chat' ? <MessagesPane /> : <ContactDetail />}
+      </main>
     </div>
   );
 }
