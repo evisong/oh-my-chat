a8ccc2646c194ec0350613f8a12db7a693b59a5f
EviSONG <evisong@live.com>
2024-02-18 00:14:33 +0800

    第4章 利用state实现发新消息功能

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,30 +1,11 @@
-import reactLogo from '../assets/react.svg';
-
-const MessageList = () => (
+const MessageList = ({ messages }) => (
   <ul className="message-list">
-    <li className="from-me">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">你好React！</p>
-    </li>
-    <li>
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">你好React！</p>
-    </li>
-    <li className="from-me">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">
-        欢迎阅读《现代React
-        Web应用设计开发实践》，你现在看到的是本书的样例应用。
-      </p>
-    </li>
-    <li>
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">这款应用有名字吗？</p>
-    </li>
-    <li className="from-me">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">有的，就叫《我聊》。</p>
-    </li>
+    {messages.map(({ id, content, from, fromAvatar }) => (
+      <li key={id} className={from === 'me' ? 'from-me' : undefined}>
+        <img src={fromAvatar} className="avatar" alt="头像" />
+        <p className="message">{content}</p>
+      </li>
+    ))}
   </ul>
 );
 
