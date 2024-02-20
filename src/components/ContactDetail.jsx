487bf50bbd2f4fbf8ecadf043671021ff14f198b
EviSONG <evisong@live.com>
2024-02-20 19:54:17 +0800

    第4章 用context实现视图切换（续）

--- src/components/ContactDetail.jsx
+++ src/components/ContactDetail.jsx
@@ -1,17 +1,24 @@
+import React from 'react';
+import NavigationContext from '../context/NavigationContext.jsx';
 import reactLogo from '../assets/react.svg';
 
-const ContactDetail = () => (
-  <>
-    <div className="contact-detail">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <div className="contact-name">小美</div>
-    </div>
-    <div className="contact-actions">
-      <button className="primary-button">发消息</button>
-      <button className="secondary-button">修改联系人</button>
-      <button className="secondary-button">删除联系人</button>
-    </div>
-  </>
-);
+const ContactDetail = () => {
+  const { gotoChatView } = React.useContext(NavigationContext);
+  return (
+    <>
+      <div className="contact-detail">
+        <img src={reactLogo} className="avatar" alt="头像" />
+        <div className="contact-name">小美</div>
+      </div>
+      <div className="contact-actions">
+        <button onClick={gotoChatView} className="primary-button">
+          发消息
+        </button>
+        <button className="secondary-button">修改联系人</button>
+        <button className="secondary-button">删除联系人</button>
+      </div>
+    </>
+  );
+};
 
 export default ContactDetail;
