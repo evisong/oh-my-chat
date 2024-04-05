209b295ced5623d20db54c542cd9846ad42b4de9
EviSONG <evisong@live.com>
2024-04-05 23:26:20 +0800

    Revert "第6章 显示消息发送时间"
    This reverts commit 4f947568e83e3a99b78283086bcf6ea0de641529.

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,31 +1,12 @@
-import React from 'react';
-
-const MessageList = ({ messages }) => {
-  const dataTimeFormat = new Intl.DateTimeFormat(undefined, {dateStyle:'medium', timeStyle: 'medium'});
-
-  return (
-    <ul className="message-list">
-      {messages.map(({ id, content, from, fromAvatar, sentTime }, idx, arr) => (
-        <React.Fragment key={id}>
-          {(idx > 0 && shouldHideSentTime(arr[idx - 1], arr[idx])) || (
-            <li className="timestamp">
-              {dataTimeFormat.format(new Date(sentTime))}
-            </li>
-          )}
-          <li className={from === 'me' ? 'from-me' : undefined}>
-            <img src={fromAvatar} className="avatar" alt="头像" />
-            <p className="message">{content}</p>
-          </li>
-        </React.Fragment>
-      ))}
-    </ul>
-  );
-};
-
-function shouldHideSentTime(prevMsg, currMsg) {
-  const prevTime = new Date(prevMsg.sentTime).getTime();
-  const currTime = new Date(currMsg.sentTime).getTime();
-  return currTime - prevTime < 1000 * 60 * 3;
-}
+const MessageList = ({ messages }) => (
+  <ul className="message-list">
+    {messages.map(({ id, content, from, fromAvatar }) => (
+      <li key={id} className={from === 'me' ? 'from-me' : undefined}>
+        <img src={fromAvatar} className="avatar" alt="头像" />
+        <p className="message">{content}</p>
+      </li>
+    ))}
+  </ul>
+);
 
 export default MessageList;
