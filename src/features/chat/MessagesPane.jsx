70474fac96780b3b9d9cd3f0dd82e2a4dcab4df4
EviSONG <evisong@live.com>
2024-08-12 14:53:04 +0800

    第18章 用LangChain加入系统提示语和对话历史

--- src/features/chat/MessagesPane.jsx
+++ src/features/chat/MessagesPane.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useState, useRef } from 'react';
 import { css } from '@linaria/core';
 import { ChatOllama } from '@langchain/ollama';
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from '@langchain/core/prompts';
+import {
+  AIMessage,
+  HumanMessage,
+  SystemMessage,
+} from '@langchain/core/messages';
 import { produce } from 'immer';
 import reactLogo from '#assets/react.svg';
 import MessageTopMenu from './MessageTopMenu.jsx';
@@ -78,8 +87,22 @@ const MessagesPane = ({ selectedThreadId }) => {
       return [...currentMessages, newMessage];
     });
     if (selectedThreadId === 100) {
+      const promptTemplate = ChatPromptTemplate.fromMessages([
+        new SystemMessage(
+          'You are a React.js expert. ' +
+            'Users are learning React.js and will ask you questions. ' +
+            'Please provide helpful answers in Simplified Chinese language. ' +
+            'Do not include markdown or other formatting syntax in your answer.'
+        ),
+        new MessagesPlaceholder('msgs'),
+      ]);
       const llm = new ChatOllama({ model: 'llama3.1' });
-      const stream = await llm.stream(content);
+      const chain = promptTemplate.pipe(llm);
+      const msgs = messages.map((m) =>
+        m.from === 'me' ? new HumanMessage(m.content) : new AIMessage(m.content)
+      );
+      msgs.push(new HumanMessage(content));
+      const stream = await chain.stream({ msgs });
       setMessages((currentMessages) => {
         const newMessage = {
           id: currentMessages.length + 1,
