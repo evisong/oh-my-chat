820bd9d83c603973bf937e5344193dfcb9a1cc41
EviSONG <evisong@live.com>
2025-05-11 18:39:22 +0800

    ch04_3_3 用state实现发送新消息功能

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,29 +1,11 @@
-import reactLogo from '../assets/react.svg';
-
-const MessageList = () => (
+const MessageList = ({ messages }) => (
   <ul className="message-list">
-    <li className="from-me">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">你好React！</p>
-    </li>
-    <li>
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">你好React！</p>
-    </li>
-    <li className="from-me">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">
-        欢迎学习React Web应用开发，你现在看到的是本书的聊天应用。
-      </p>
-    </li>
-    <li>
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">这款应用有名字吗？</p>
-    </li>
-    <li className="from-me">
-      <img src={reactLogo} className="avatar" alt="头像" />
-      <p className="message">有的，就叫《我聊》。</p>
-    </li>
+    {messages.map(({ id, content, from, fromAvatar }) => (
+      <li key={id} className={from === 'me' ? 'from-me' : undefined}>
+        <img src={fromAvatar} className="avatar" alt="头像" />
+        <p className="message">{content}</p>
+      </li>
+    ))}
   </ul>
 );
 
--- src/components/MessagesPane.jsx
+++ src/components/MessagesPane.jsx
@@ -1,13 +1,69 @@
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
 import MessageTopMenu from './MessageTopMenu.jsx';
 import MessageList from './MessageList.jsx';
 import NewMessageForm from './NewMessageForm.jsx';
 
-const MessagesPane = () => (
-  <>
-    <MessageTopMenu />
-    <MessageList />
-    <NewMessageForm />
-  </>
-);
+const mockMessages = [
+  {
+    id: 1,
+    content: '你好React！',
+    from: 'me',
+    fromAvatar: reactLogo,
+    sentTime: '2023-11-04',
+  },
+  {
+    id: 2,
+    content: '你好React！',
+    from: '小白',
+    fromAvatar: reactLogo,
+    sentTime: '2023-11-05',
+  },
+  {
+    id: 3,
+    content: '欢迎学习React Web应用开发，你现在看到的是本书的聊天应用。',
+    from: 'me',
+    fromAvatar: reactLogo,
+    sentTime: '2023-11-06',
+  },
+  {
+    id: 4,
+    content: '这款应用有名字吗？',
+    from: '小白',
+    fromAvatar: reactLogo,
+    sentTime: '2023-11-07',
+  },
+  {
+    id: 5,
+    content: '有的，就叫《我聊》。',
+    from: 'me',
+    fromAvatar: reactLogo,
+    sentTime: '2023-11-08',
+  },
+];
+
+const MessagesPane = () => {
+  const [messages, setMessages] = useState(mockMessages);
+  const handleSubmitMessage = (content) => {
+    setMessages(currentMessages => {
+      const newMessage = {
+        id: currentMessages.length + 1,
+        content,
+        from: 'me',
+        fromAvatar: reactLogo,
+        sentTime: new Date().toISOString(),
+      };
+      return [...currentMessages, newMessage];
+    });
+  };
+
+  return (
+    <>
+      <MessageTopMenu />
+      <MessageList messages={messages} />
+      <NewMessageForm onSubmitMessage={handleSubmitMessage} />
+    </>
+  );
+};
 
 export default MessagesPane;
