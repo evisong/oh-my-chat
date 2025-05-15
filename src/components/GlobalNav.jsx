8e3c681a30be8c1833b0e8f4bd678bcb9ed95966
EviSONG <evisong@live.com>
2025-05-15 22:44:50 +0800

    ch08_4_3 用linaria嵌套选择器改写导航栏样式

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { css } from '@linaria/core';
+import { css, cx } from '@linaria/core';
 import NavigationContext from '../context/NavigationContext.jsx';
 import reactLogo from '../assets/react.svg';
 import contactIcon from '../assets/icon-contact.svg';
@@ -13,6 +13,40 @@ const navStyles = css`
   justify-content: flex-start;
   align-items: center;
 `;
+const myAvatarStyles = css`
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  background-color: #eeeeee;
+`;
+const topNavStyles = css`
+  margin: 16px 0;
+  padding: 0;
+  list-style: none;
+  width: 100%;
+
+  & > li {
+    height: 80px;
+    & > a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+
+      &:hover,
+      &:active {
+        background-color: #ffffff11;
+      }
+      & > img {
+        width: 2rem;
+        height: 2rem;
+      }
+    }
+  }
+`;
+const activeStyles = css`
+  background-color: #ffffff33;
+`;
 
 const GlobalNav = () => {
   const { activeView, gotoChatView, gotoContactView } =
@@ -20,14 +54,14 @@ const GlobalNav = () => {
 
   return (
     <nav className={navStyles}>
-      <img src={reactLogo} className="my-avatar" alt="我的头像" />
-      <ul className="top-nav">
-        <li className={activeView === 'chat' ? 'active' : undefined}>
+      <img src={reactLogo} className={myAvatarStyles} alt="我的头像" />
+      <ul className={topNavStyles}>
+        <li className={cx(activeView === 'chat' && activeStyles)}>
           <a href="#" onClick={gotoChatView}>
             <img src={messageIcon} alt="消息" />
           </a>
         </li>
-        <li className={activeView === 'contact' ? 'active' : undefined}>
+        <li className={cx(activeView === 'contact' && activeStyles)}>
           <a href="#" onClick={gotoContactView}>
             <img src={contactIcon} alt="联系人" />
           </a>
